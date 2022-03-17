699d90e7d94f587a8dcd26544727b5cce96dfc34
alrolando <alrolando@gmail.com>
2022-03-16 21:28:46 -0600

    Primera app

---
+++ src/inicio.js
@@ -0,0 +1,10 @@
+function inicio() {
+    return(
+        <div>
+            <h1>!BIENVENIDO A REACT!</h1>
+            <p>Esta a punto de iniciar un viaje en que creara aplicaciones web usando las tecnologias mas recientes</p>
+        </div>
+    )
+}
+
+export default inicio;
